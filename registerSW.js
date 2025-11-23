c777a20e1259c8af7a5e96fb7c6b6905f66a232d
Esai-Keshav <Esai-Keshav@users.noreply.github.com>
2025-11-23 12:58:54 +0000

    deploy: 61422e7f136d5a9aa3f9380e934d93145b929c2a

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/diet-planner-app/sw.js', { scope: '/diet-planner-app/' })})}
